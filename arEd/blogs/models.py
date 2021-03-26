6216bb2b83ae93bbac3ef39ebdbd7b7ea01af7b2
Shreya-IITJ <75309354+Shreya-IITJ@users.noreply.github.com>
2021-03-26 18:24:53 +0530

    Update models.py

--- arEd/blogs/models.py
+++ arEd/blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    short_description = models.TextField()
+    blog_post = RichTextField()
+    
+    def __str__(self):
+        return self.title + '  ||  ' + str(self.author)
