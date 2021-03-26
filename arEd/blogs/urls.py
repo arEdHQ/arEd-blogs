4b1ad2741117b1502962457a00bb83a7477377b8
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-03-26 19:21:39 +0530

    updated

--- arEd/blogs/urls.py
+++ arEd/blogs/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='social_app/index.html')),
-    path('', include('blogs.urls')),
+
     path('accounts/', include('allauth.urls')),
 ]
