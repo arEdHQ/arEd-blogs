b28859fcad82e4dd70fecdb01265b86b53266475
Shreya <shukla.12@iitj.ac.in>
2021-03-26 20:46:38 +0530

    Update urls.py

--- arEd/blogs/urls.py
+++ arEd/blogs/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from .views import HomeView, ArticleView
 
+urlpatterns = [
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleView.as_view(),name='article-details'),
 ]
