92e8820cfc096f3eb6f7bde76b95eb6bf18da471
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-03-26 20:46:18 +0530

    updated

--- arEd/arEd/settings.py
+++ arEd/arEd/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'blogs',
     'ckeditor',
-    'django.contrib.sites',
-
 
     'allauth',
     'allauth.account',
