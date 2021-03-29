262f3c3d8d63a3a340adaf000b9640b11146e1b3
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-03-29 12:38:07 +0530

    added pages to add and edit blogs

--- arEd/blogs/models.py
+++ arEd/blogs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+from django.urls import reverse
 
 
 # Create your models here.
@@ -19,3 +20,6 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog-detail', args=(str(self.id)))
--- arEd/blogs/views.py
+++ arEd/blogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Blog
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -20,6 +20,17 @@ class BlogView(DetailView):
         obj.save()
         return obj
 
+
+class AddBlogView(CreateView):
+    model = Blog
+    template_name = 'add_blog.html'
+    fields = '__all__'
+
+
+class UpdateBlogView(UpdateView):
+    model = Blog
+    template_name = 'update_blog.html'
+    fields = ['title', 'short_description', 'blog_content']
 
 # def BlogPostLike(request, pk):
 #     post = get_object_or_404(Blog, id=)
