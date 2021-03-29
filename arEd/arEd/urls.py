fe3f0037723328a095addf5408b73e447506eab4
Shreya <shukla.12@iitj.ac.in>
2021-03-29 16:22:55 +0530

    improved image field

--- arEd/arEd/urls.py
+++ arEd/arEd/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from blogs.views import HomeView
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
