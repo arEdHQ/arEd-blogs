97931c9dec3ff67aecda625059e851ef9eda7c8d
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-03-31 14:17:37 +0530

    Delete arEd directory

--- arEd/blogs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import BlogView, HomeView, AddBlogView, UpdateBlogView, DeleteBlogView
-urlpatterns = [
-    path('', HomeView.as_view(), name="home"),
-    path('blog/<int:pk>', BlogView.as_view(), name='blog-detail'),
-    path('add_blog/', AddBlogView.as_view(), name='add_blog'),
-    path('blog/edit/<int:pk>', UpdateBlogView.as_view(), name='update'),
-    path('blog/<int:pk>/remove', DeleteBlogView.as_view(), name='delete'),
-]
