1cb684892831e1966c082b253e0a1821c5471461
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-04-03 00:10:20 +0530

    Update urls.py

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import BlogView, HomeView, AddBlogView
-from .views import UpdateBlogView, DeleteBlogView
+from . import views
+from .views import BlogView, HomeView, AddBlogView, UpdateBlogView, DeleteBlogView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add_blog/', AddBlogView.as_view(), name='add_blog'),
     path('blog/edit/<int:pk>', UpdateBlogView.as_view(), name='update'),
     path('blog/<int:pk>/remove', DeleteBlogView.as_view(), name='delete'),
+    path("logout", views.logout_request, name="logout"),
 ]
