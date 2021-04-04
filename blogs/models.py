b6c70c473bbfbef69e80af418c828f71088623f8
shivalee12 <77015285+shivalee12@users.noreply.github.com>
2021-04-04 22:22:10 +0530

    basic css added

--- blogs/models.py
+++ blogs/models.py
@@ -11,19 +11,21 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title_image = models.ImageField(
-        upload_to = 'blog/',
+        upload_to='blog/',
         blank=True,
         null=True,
         default='blog/default.png',
-        help_text = 'This image will be displayed as the title image of the blog.'
+        help_text='This image will be displayed as the title image of the blog.'
     )
     short_description = models.TextField()
     blog_content = RichTextUploadingField()
     published_date = models.DateTimeField(auto_now_add=True)
     blog_views = models.IntegerField(default=0)
+    blog_likes = models.ManyToManyField(User, related_name='likes')
 
     def __str__(self):
         return self.title
 
+# this redirects post button to blog-detail page
     def get_absolute_url(self):
         return reverse('blog-detail', args=(str(self.id)))
