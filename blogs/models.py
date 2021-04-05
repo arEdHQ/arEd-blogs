ab7aab6b2f5724bb756d7815dbfe647d5f3aa63a
Shreya-IITJ <75309354+Shreya-IITJ@users.noreply.github.com>
2021-04-05 14:29:30 +0530

    Update models.py

--- blogs/models.py
+++ blogs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -20,15 +21,25 @@ class Blog(models.Model):
     short_description = models.TextField()
     blog_content = RichTextUploadingField()
     published_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, blank=True)
     blog_views = models.IntegerField(default=0)
     blog_likes = models.ManyToManyField(User, related_name='likes')
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            if Blog.objects.filter(title=(self.title)).exists():
+                count = Blog.objects.filter(title=(self.title)).count()
+                self.slug = "%s-%s" % (slugify(kwargs.pop('title', self.title)), count+1)
+            else:
+                self.slug = slugify(kwargs.pop('title', self.title))
+        return super(Blog, self).save(*args, **kwargs)
 
 # this redirects post button to blog-detail page
     def get_absolute_url(self):
-        return reverse('home', args=(str(self.id)))
+        return reverse('home')
 
     def number_of_likes(self):
         return self.blog_likes.count()
